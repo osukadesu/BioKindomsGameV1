749f4b43bce4ec759031d7bbe63b8f5893e87672
Oscar David Serge Blanco <64621867+osukadesu@users.noreply.github.com>
2024-08-26 10:14:36 -0500

    Update ShootLogic.cs

--- Assets/MyAssets/Scripts/Game/Character/ShootLogic.cs
+++ Assets/MyAssets/Scripts/Game/Character/ShootLogic.cs
@@ -2,34 +2,26 @@ using System.Collections;
 using UnityEngine;
 public class ShootLogic : MonoBehaviour
 {
-    public GameObject bullet, newBullet;
+    [SerializeField] GameObject bullet;
     public Transform spawnBullet;
     public float shootForce = 1500f, shootRate = 0.5f, shootRateTime = 0.5f;
     public bool canShoot;
-    void Start()
-    {
-        canShoot = false;
-    }
+    void Start() => canShoot = false;
     void Update()
     {
         if (canShoot && Input.GetButtonDown("Fire1"))
         {
             if (Time.time > shootRateTime)
             {
-                newBullet = Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
-                newBullet.GetComponent<Rigidbody>().AddForce(spawnBullet.forward * shootForce);
+                bullet = ShootingPool.shootingPool.RequestBullet();
+                bullet.transform.position = spawnBullet.position;
+                bullet.GetComponent<Rigidbody>().AddForce(spawnBullet.forward * shootForce);
                 shootRateTime = Time.time + shootRate;
             }
         }
     }
-    public void DestroyNewBullet()
-    {
-        Destroy(newBullet, 0f);
-    }
-    public void SetCanShoot()
-    {
-        StartCoroutine(IECanshot(true));
-    }
+    public void HideNewBullet() => bullet.SetActive(false);
+    public void SetCanShoot() => StartCoroutine(IECanshot(true));
     IEnumerator IECanshot(bool _canShoot)
     {
         yield return new WaitForSeconds(1f);
