458ff3871c7a5152783c4e338370d0e5e4bc726f
Oscar David Serge Blanco <64621867+osukadesu@users.noreply.github.com>
2024-11-03 16:31:50 -0500

    Update ChanginLevelCollision.cs

--- Assets/MyAssets/Scripts/Game/Menu/ChanginLevelCollision.cs
+++ Assets/MyAssets/Scripts/Game/Menu/ChanginLevelCollision.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 public class ChanginLevelCollision : MonoBehaviour
 {
     [SerializeField] LevelSystem levelSystemV2;
+    [SerializeField] ItemObject itemObject;
     void Awake()
     {
         levelSystemV2 = FindObjectOfType<LevelSystem>();
+        itemObject = FindObjectOfType<ItemObject>();
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             levelSystemV2.ChangeLevel();
+            itemObject.HideTextV2();
         }
     }
 }
