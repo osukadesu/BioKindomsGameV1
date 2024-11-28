1d0370e0e1771e71a654bc43cff060678dcfcdce
Oscar David Serge Blanco <64621867+osukadesu@users.noreply.github.com>
2024-11-27 21:54:33 -0500

    AuthInit

--- Assets/MyAssets/Scripts/Firebase/FirebaseAuthManager.cs
+++ Assets/MyAssets/Scripts/Firebase/FirebaseAuthManager.cs
@@ -20,7 +20,7 @@ public class FirebaseAuthManager : MonoBehaviour
     void Start()
     {
         InitializeFirebase();
-        SetALert(0, false);
+        SetALert(0, false, 1, false);
     }
     private void InitializeFirebase()
     {
@@ -30,14 +30,15 @@ public class FirebaseAuthManager : MonoBehaviour
             auth = FirebaseAuth.DefaultInstance;
         });
     }
-    void SetALert(int index, bool myBool)
+    void SetALert(int index, bool myBool, int index2, bool myBool2)
     {
         imageAlert[index].SetActive(myBool);
+        imageAlert[index2].SetActive(myBool2);
         alertModal.SetBool("showAlert", myBool);
     }
     public void CloseAlert()
     {
-        SetALert(0, false);
+        SetALert(0, false, 1, false);
     }
     public void Login()
     {
@@ -45,53 +46,39 @@ public class FirebaseAuthManager : MonoBehaviour
     }
     IEnumerator IELogin()
     {
-        SetALert(0, true);
-        string email = emailInputField.text;
+        SetALert(0, false, 1, false);
+        string email = emailInputField.text + "@gmail.com";
         string password = passwordInputField.text;
-        if (email == "")
+        var loginTask = auth.SignInWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(() => loginTask.IsCompleted);
+        if (loginTask.Exception != null)
         {
-            messageText.text = "El correo es requerido";
-        }
-        if (password == "")
-        {
-            messageText.text = "La contraseña es requerida";
-        }
-        if (email == "" && password == "")
-        {
-            messageText.text = "Correo y contraseña requeridos";
+            Debug.LogError(loginTask.Exception);
+            FirebaseException firebaseException = loginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError authError = (AuthError)firebaseException.ErrorCode;
+            string errorMessage = "Error al iniciar sesión porque: ";
+            errorMessage += authError switch
+            {
+                AuthError.InvalidEmail => "El correo no es valido",
+                AuthError.WrongPassword => "La contraseña es erronea",
+                AuthError.MissingEmail => "No hay correo",
+                AuthError.MissingPassword => "No hay contraseña",
+                AuthError.UserNotFound => "El usuario no existe",
+                _ => "La cuenta no existe",
+            };
+            SetALert(0, true, 1, false);
+            messageText.text = errorMessage;
         }
         else
         {
-            var loginTask = auth.SignInWithEmailAndPasswordAsync(email, password);
-            yield return new WaitUntil(() => loginTask.IsCompleted);
-            if (loginTask.Exception != null)
-            {
-                Debug.LogError(loginTask.Exception);
-                FirebaseException firebaseException = loginTask.Exception.GetBaseException() as FirebaseException;
-                AuthError authError = (AuthError)firebaseException.ErrorCode;
-                string errorMessage = "Error al iniciar sesión porque: ";
-                errorMessage += authError switch
-                {
-                    AuthError.InvalidEmail => "El correo no es valido",
-                    AuthError.WrongPassword => "La contraseña es erronea",
-                    AuthError.MissingEmail => "No hay correo",
-                    AuthError.MissingPassword => "No hay contraseña",
-                    _ => "La cuenta no existe",
-                };
-                SetALert(0, true);
-                messageText.text = errorMessage;
-            }
-            else
-            {
-                AuthResult result = loginTask.Result;
-                FirebaseUser user = result.User;
-                SetALert(1, true);
-                messageText.text = "Inicio de sesión exitoso!";
-                yield return new WaitForSeconds(1f);
-                CloseAlert();
-                ClearLoginInputs();
-                SceneManager.LoadScene(2);
-            }
+            AuthResult result = loginTask.Result;
+            FirebaseUser user = result.User;
+            SetALert(0, false, 1, true);
+            messageText.text = "Inicio de sesión exitoso!";
+            CloseAlert();
+            ClearLoginInputs();
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene(2);
         }
     }
     void ClearLoginInputs()
